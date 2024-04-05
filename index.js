d8014898c2ad5cf437b4efe7c0fb4998200ca3f1
AnthonyG-Dev <giterugicheru@gmail.com>
2024-04-05 10:56:31 +0300

    Understand arrow functions, its implicit return when in a single line and other tweaks

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function (num) {
+  return 2000 / 100;
+};
+//solution
+// let divide = () => 2000 / 100;
+
+const square = (x) => x ** 2;
+//solution
+// let square = x => x * x;
+
+const add = (num, num2) => {
+  return num + num2;
+};
+//solution
+// let add = (a, b) => a + b;
